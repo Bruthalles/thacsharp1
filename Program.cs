18ce7386b64c98d347c4ecdae03c0eba0944b028
bruthalles <thallesgmkr@gmail.com>
2024-11-10 00:17:25 -0300

    primeira vez no c#

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+    string msg01 = "hello world";
+    string msg02 = "bye world";
+    string tudo = msg01 + msg02;
+    bool diabom = true;
+
+    for(int i=0; i==5; i++){
+
+        if(diabom){
+            Console.WriteLine(msg01);
+            diabom = !diabom;
+            
+        }
+        else{
+            Console.WriteLine(msg02);
+            diabom = !diabom;
+            
+        }
+
+    }
+
+    Console.WriteLine(tudo);
+
+            
+
+
+    
