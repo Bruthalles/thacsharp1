9ab60ba51f926826cb52b662d839628931030fad
bruthalles <thallesgmkr@gmail.com>
2024-11-10 19:55:56 -0300

    criando binario em c#

---
+++ binario.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Reflection.Metadata;
+
+
+class Binario{
+
+    int tam = 100;
+    TypeDefinition struct {
+        int topo;
+        int bin[tam];
+    }pilhavet;
+
+    void empilha(int resto, pilhavet *p){}
+    
+        
+    
+    static void main(){
+    }
+}
