e3771fcf062ef72d6b3748e6ffcd73a4dac0eb24
bruthalles <thallesgmkr@gmail.com>
2024-12-02 00:42:33 -0300

    restarting repostorio

--- obj/Debug/net8.0/thac#1.AssemblyInfo.cs
+++ obj/Debug/net8.0/thac#1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c6251d4b575570c16a7addbc57f294b9f05482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab60ba51f926826cb52b662d839628931030fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
