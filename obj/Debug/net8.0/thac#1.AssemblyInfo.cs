858219e28fd160b3950019e28cb48fd5b66bbebd
bruthalles <thallesgmkr@gmail.com>
2024-12-07 15:11:28 -0300

    arquivo caneta criado

--- obj/Debug/net8.0/thac#1.AssemblyInfo.cs
+++ obj/Debug/net8.0/thac#1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab60ba51f926826cb52b662d839628931030fad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdc34230c31ad409ee85dcbc2775085049e58f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
