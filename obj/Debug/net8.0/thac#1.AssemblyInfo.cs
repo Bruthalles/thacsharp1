b84739c934a2469c1e43da0452071c52ed672159
Bruthalles <thallesgmkr@gmail.com>
2025-03-04 15:53:02 -0300

    beybe

--- obj/Debug/net8.0/thac#1.AssemblyInfo.cs
+++ obj/Debug/net8.0/thac#1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61682dcfb79528627162e995ee9cf650a1d69f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e376f6185a8291bcf712c7fe7be0201301818a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thac#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
