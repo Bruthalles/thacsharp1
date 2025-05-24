aa7bf1f84c365ccb3f3d37d9cc80be9baf6bbf66
Bruthalles <thallesgmkr@gmail.com>
2025-05-24 17:47:46 -0300

    Trim e Length

--- livrocsharp/cap3/FuncoesString.cs
+++ livrocsharp/cap3/FuncoesString.cs
@@ -1,21 +1,29 @@
-using System;
+﻿using System;
 using System.Linq;
 using static System.Console;
 
-namespace FuncoesString{
-    class EspacoTrim{
-
-        static void Main(string[] args){
-
-            int cpf = 123456789;
-            string nome = "         thalles      ";
-            string sobrenome = "     roberto        ";
+namespace cap3{
+    class FuncoesString
+    {
+        static void Main(string[] args)
+        {
+            string nome = "      thalles        ";
+            string sobrenome = "     roberto    ";
             string full_name = nome + sobrenome;
+            string full_name_trim = full_name.Trim();
 
-            WriteLine("------Funções de Textos------");
-            WriteLine("TRIM - retira os espaços vazios antes e depois da string");
-            WriteLine($"Nome original: ({full_name})");
-            WriteLine($"Nome sem espaço: {full_name.Trim()}.");
+            WriteLine("-----Funções de texto-----");
+            WriteLine("TRIM remove espaços antes e depois da string");
+            WriteLine($"Nome original: {full_name}.");
+            WriteLine($"Nome sem espaço: {full_name_trim}.");
+
+            WriteLine("Length retorna a quantidade de caracteres");
+            WriteLine($"Tamanho original: {full_name.Length}");
+            WriteLine($"Tamanho sem espaços: {full_name_trim.Length}\n");
+
+
+
         }
-    }
+    }    
+    
 }
