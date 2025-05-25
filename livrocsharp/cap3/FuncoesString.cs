6234f3e13c098b519372b0c194807d03d214ce6d
Bruthalles <thallesgmkr@gmail.com>
2025-05-24 21:39:51 -0300

    comparando strings

--- livrocsharp/cap3/FuncoesString.cs
+++ livrocsharp/cap3/FuncoesString.cs
@@ -21,7 +21,26 @@ namespace cap3{
             WriteLine($"Tamanho original: {full_name.Length}");
             WriteLine($"Tamanho sem espaços: {full_name_trim.Length}\n");
 
+            string nomeUpper = "AIRTON SENNA";
+            string nomeLower = "airton senna";
 
+            //comparação 1
+            if (nomeUpper == nomeLower)
+                WriteLine("1 - nomes iguais");
+            else
+                WriteLine("1 - nomes diferentes");
+
+            //comparação 2
+            if (nomeUpper.ToLower() == nomeLower)
+                WriteLine("2 - nomes iguais");
+            else
+                WriteLine("2 - nomes diferentes");
+
+            //comparação 3
+            if (nomeUpper.Equals(nomeLower, StringComparison.OrdinalIgnoreCase))
+                WriteLine("3 - nomes iguais");
+            else
+                WriteLine("3 - nomes diferentes");
 
         }
     }    
