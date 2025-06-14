76b29d69280b1a1d79061a70f4ad51358b1c009d
Bruthalles <thallesgmkr@gmail.com>
2025-06-14 11:12:03 -0300

    create class gerente

---
+++ SuperMercado/mvc/models/Negocios/Gerente.cs
@@ -0,0 +1,14 @@
+namespace gerente{
+    /// <summary>
+    /// Cria Gerente com nome, id e senha
+    /// </summary>
+    public class Gerente{
+        private string nome;
+        private string id;
+        private string senha;
+
+        public string Nome{get;set;}
+        public string Id{get;set;}
+        public string senha{get;set;}
+    }
+}
