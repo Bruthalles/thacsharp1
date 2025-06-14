cdaa519a73f4c0766d480fc2079f5f00d1df0fc1
Bruthalles <thallesgmkr@gmail.com>
2025-06-14 16:21:02 -0300

    updating project

---
+++ livrocsharp/cap3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using functrim;
+using funclength;
+using funcUpLow;
+using funcRemove;
+using funcReplace;
+using funcSplit;
+using funcSubstring;
+using funcNullEmpty;
+using funcDates;
+
+namespace cap3{
+    class FuncoesString{
+        static void Main(string[] args){
+
+            WriteLine("\n-----Funções de texto-----\n");
+
+            TrimClass.fTrim();
+            LengClass.fLength();
+            UpLowClass.fUpLow();
+            RemoveClass.fRemove();
+            ReplaceClass.fReplace();
+            SplitClass.fSplit();
+            SubstringClass.fSubstring();
+            NullEmptyClass.fNullEmpty();
+            DatesClass.fDates();
+            
+        }
+    }    
+}
