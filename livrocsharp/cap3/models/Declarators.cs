3d74051b12b2287ec4776bb848b40bd9f24d8abf
Bruthalles <thallesgmkr@gmail.com>
2025-06-14 16:19:01 -0300

    modularizando variáveis

---
+++ livrocsharp/cap3/models/Declarators.cs
@@ -0,0 +1,28 @@
+namespace vars{
+    public static class Contents{
+
+        //strings: 
+        public static string? nome = "      thalles        ";
+        public static string? sobrenome = "     roberto    ";
+        public static string? fullName = nome + sobrenome;
+        public static string? fullName_trim = fullName.Trim();
+
+        public static string? empresa = "Microsoft Corporation";
+
+        public static string? nomeUpper = "AIRTON SENNA";
+        public static string? nomeLower = "airton senna";
+
+        // vetores:
+        public static string[] list_nomes = { "Silvio Santos", "José Bezerra", "Getúlio Vargas" };
+
+        public static string nivelLivro = "Exercicios acompanhados do livro C# para iniciantes";
+
+        public static string[] cesta = {"5 laranjas","10 goiabas vermelhas","5  pêssegos doces","10 bananas d'água"};
+
+        // datas:
+        public static int dia = 12;
+        public static int mes = 05;
+        public static int ano = 2021;
+        
+    }
+}
