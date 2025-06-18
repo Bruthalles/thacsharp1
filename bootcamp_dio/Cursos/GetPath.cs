1e5fd172fad7c45c2a9f8de3c612a62850010e9a
Bruthalles <thallesgmkr@gmail.com>
2025-06-18 11:24:38 -0300

    funcoes de arquivo

---
+++ bootcamp_dio/Cursos/GetPath.cs
@@ -0,0 +1,45 @@
+
+namespace getpath{
+    public static class GetPath{
+
+    //diretorio onde fica o executavel - /Projeto/bin/Debug/net9.0/
+    private static string basePath = AppContext.BaseDirectory;
+
+    // Volta alguns níveis até chegar na raiz do projeto 
+    private static string raizProjeto = Directory.GetParent(basePath)?.Parent?.Parent?.Parent?.FullName;
+
+    public static string caminhoArquivo;
+
+    /// <summary>
+    /// utiliza arquivo na pasta raiz do projeto 
+    /// </summary>
+    /// <param name="nomeArquivo">escolha um nome e extensão</param>
+    /// <returns>caminho completo</returns>
+    public static string ObterCaminhoArquivo(string nomeArquivo){
+        caminhoArquivo = $"{raizProjeto}/{nomeArquivo}";
+        return caminhoArquivo;
+        }
+
+    /// <summary>
+    /// cria arquivo na raiz do projeto
+    /// </summary>
+    /// <param name="nomeArquivo">nome do arquivo para criar</param>
+    /// <param name="conteudo">objetos convertidos para lista que salva no json</param>
+    
+    public static void CriarArquivo(string nomeArquivo, List<string> conteudo){
+            
+        // Monta o caminho até um arquivo .json
+        caminhoArquivo = Path.Combine(raizProjeto!, nomeArquivo);
+
+        try{
+            using(StreamWriter writer = new StreamWriter(caminhoArquivo,append:true)){
+                writer.WriteLine(conteudo);
+                }
+                Console.WriteLine($"Arquivo e conteudo salvo com sucesso em {caminhoArquivo}");
+            }
+            catch(Exception ex){
+                Console.WriteLine($"Erro ao salvar conteudo", ex);
+            }
+        }
+    }
+}
