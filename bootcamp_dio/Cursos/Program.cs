67caa1c20011274d128ee2adbbf842a332174c05
Bruthalles <thallesgmkr@gmail.com>
2025-06-18 11:24:59 -0300

    fix leitura arquivo

--- bootcamp_dio/Cursos/Program.cs
+++ bootcamp_dio/Cursos/Program.cs
@@ -1,19 +1,33 @@
 ﻿using models.pessoa;
 using models.curso;
+using static getpath.GetPath;
+using static System.Console;
+using System;
+using System.Linq;
+using System.IO;
 
 namespace program{
-    class Program{
+    public class Program{
+
+        public static string[] linhas = File.ReadAllLines(ObterCaminhoArquivo("textoleitura.txt"));
         public static void Main(string[] args){
-            Pessoa p1 = new Pessoa(nome:"thalles",sobrenome:"Brumatti",idade:20);
-            //p1.Apresentar();
 
-            Pessoa p2 = new Pessoa(nome:"josé",sobrenome:"bezerra");
-
-            Curso cursoDeIngles = new Curso();
-            cursoDeIngles.Nome = "Curso de ingles";
-            cursoDeIngles.AdicionarAlunos(p1);
-            cursoDeIngles.AdicionarAlunos(p2);
-            cursoDeIngles.ListarAlunos();
+            foreach(string linha in linhas){
+                WriteLine(linha);
+            }
         }
     }
 }
+
+
+
+            // Pessoa p1 = new Pessoa(nome:"thalles",sobrenome:"Brumatti",idade:20);
+            // //p1.Apresentar();
+
+            // Pessoa p2 = new Pessoa(nome:"josé",sobrenome:"bezerra");
+
+            // Curso cursoDeIngles = new Curso();
+            // cursoDeIngles.Nome = "Curso de ingles";
+            // cursoDeIngles.AdicionarAlunos(p1);
+            // cursoDeIngles.AdicionarAlunos(p2);
+            // cursoDeIngles.ListarAlunos();
